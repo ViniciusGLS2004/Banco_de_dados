244ebbd1c2c7f8300b2ac9f68a7fedd5e43a9d77
ViniciusGLS2004 <120745001+ViniciusGLS2004@users.noreply.github.com>
2023-09-06 09:29:13 -0300

    Update cs_veiculo.sql

--- cs_veiculo.sql
+++ cs_veiculo.sql
@@ -11,17 +11,26 @@ create table veiculo (
     vei_estado varchar(100) not null,
     vei_kmrodados decimal (10, 2) not null, -- no codigo vai aparecer o nome 'decimal'e o numero, mas so assim para os zeros depois do '.' aparecem no banco de dados
     vei_leilao varchar(100),
-    vei_cod_placa varchar(100) not null,
+    vei_placa varchar(100) not null,
     vei_tipo_combustivel varchar(100) not null,
     vei_direcao varchar(100) not null,
     vei_marca varchar(100) not NULL,
 	vei_modelo varchar(100) NOT NULL,
 	vei_tipo_desempenho varchar(100) not null,
 	vei_desempenho varchar(100) not null,
-    vei_preco DECIMAL(10, 2) not null
+    vei_preco DECIMAL(10, 2) not null,
+    vei_imagem LONGBLOB NOT NULL
 );
 select * from veiculo;
- select * from forma_pagamento;
+     select * from forma_pagamento;
+ select * from imagens;
+ 
+CREATE TABLE imagens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    dados_imagem LONGBLOB NOT NULL
+);
+
 
 -- Tabela para armazenar informações das formas de pagamento
 create table forma_pagamento (
@@ -30,5 +39,7 @@ create table forma_pagamento (
 	pag_fk_veiculo int not null,
     foreign key (pag_fk_veiculo) references veiculo (vei_id)
 );
+
+
 
 
